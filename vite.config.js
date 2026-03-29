3197260cbf98402e91053a9fdb46d4156d80843e
Marcos1203pro3 <gamargabriel@gmail.com>
2026-03-29 13:27:56 -0500

    Remove proxy from vite config

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
-    }
+    port: 5173
   }
 })
