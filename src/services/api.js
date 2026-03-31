a46bdcae9322c77eff42e6a32f6b3f09c06c2d39
MiguelRicardo2003 <miguelricardomm27@gmail.com>
2026-03-31 16:32:06 -0500

    refactor(api):simplificar axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://restaurante-backend-h125.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 api.interceptors.request.use((config) => {
